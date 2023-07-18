7334d238d811690c434ac12b21e3ff457a297e49
alexansaa <alexan.saa@gmail.com>
2023-07-17 23:21:02 -0500

    Review changes

--- modules/navigation.js
+++ modules/navigation.js
@@ -9,34 +9,31 @@ const links = document.querySelectorAll('#myLinks a');
 add.classList.add('hidden');
 contact.classList.add('hidden');
 
-// Function for showing the chosen section when a link gets clicked
-function GoToSection(e) {
-  // Name of the section to show
-  const name = e.target.textContent;
-
-  switch (name) {
-    case 'List':
-      list.classList.remove('hidden');
-      add.classList.add('hidden');
-      contact.classList.add('hidden');
-      break;
-    case 'Add New':
-      list.classList.add('hidden');
-      add.classList.remove('hidden');
-      contact.classList.add('hidden');
-      break;
-    case 'Contact':
-      list.classList.add('hidden');
-      add.classList.add('hidden');
-      contact.classList.remove('hidden');
-      break;
-    default:
-      break;
-  }
-}
-
 export default function InitializeNavigation() {
   links.forEach((section) => {
-    section.addEventListener('click', GoToSection);
+    section.addEventListener('click', (e) => {
+      // Name of the section to show
+      const name = e.target.textContent;
+
+      switch (name) {
+        case 'List':
+          list.classList.remove('hidden');
+          add.classList.add('hidden');
+          contact.classList.add('hidden');
+          break;
+        case 'Add New':
+          list.classList.add('hidden');
+          add.classList.remove('hidden');
+          contact.classList.add('hidden');
+          break;
+        case 'Contact':
+          list.classList.add('hidden');
+          add.classList.add('hidden');
+          contact.classList.remove('hidden');
+          break;
+        default:
+          break;
+      }
+    });
   });
 }
